e5266a5a178f299f7e74720651f12e12af3c244b
teoribprov <teoribprov@gmail.com>
2026-08-28 19:11:08 +0200

    Upload

---
+++ firebase-config.js.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDjpihYJAAXb9Ms5B9Pho84xd2wB_sxSVs",
+  authDomain: "teoribprov-67be0.firebaseapp.com",
+  databaseURL: "https://teoribprov-67be0-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "teoribprov-67be0",
+  storageBucket: "teoribprov-67be0.firebasestorage.app",
+  messagingSenderId: "586858590685",
+  appId: "1:586858590685:web:c207107de246bb3ac78634",
+  measurementId: "G-8XNGZRTSXP"
+};
+
+// Inicializo Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+console.log('🔥 Firebase u inicializua!');
